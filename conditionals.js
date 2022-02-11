8c58129e87682779402fa2b781c0ce338877e6fc
linzhu <lin.lyn.zhu@gmail.com>
2022-02-10 20:04:29 -0800

    add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,8 @@
 const raining = true;
-
 if (raining) {
   console.log("Don't forget your umbrella!");
 }
-
 const temperature = 12;
-
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
@@ -13,5 +10,19 @@ if (temperature < 0) {
 } else {
   console.log("Short sleeves are fine.");
 }
-
 console.log("Now you're ready to go outside!");
+
+//logical operators
+const isCitizen = true;
+const age = 26;
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea…");
+  }
+
+if (!raining) {
+    console.log("Leave your umbrella at home!");
+  }
